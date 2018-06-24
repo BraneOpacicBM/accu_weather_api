b8017d8e402d5000a380b08efc4594f720fecec5
Brane <braneopacic@gmail.com>
2018-06-24 15:13:23 +0200

    Made styling changes for the Navbar component and its children

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import Navbar from './components/Navbar/Navbar';
 import './App.scss';
+
 
 
 
@@ -7,7 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello World</h1>
+        <Navbar />
       </div>
     );
   }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import InputHolder from './InputHolder/InputHolder';
+import './Navbar.scss';
+
+const Navbar = (props) => {
+    return (
+        <div className="Navbar">
+            <InputHolder />
+        </div>
+    )
+}
+
+export default Navbar;
