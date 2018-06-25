f004fd9409de86d2967d76ba602677b09a7c545f
Brane <braneopacic@gmail.com>
2018-06-25 23:35:57 +0200

    Added logic for async fetch get request and spreading data from redux state across the app

--- src/store/reducers/reducer.js
+++ src/store/reducers/reducer.js
@@ -6,10 +6,19 @@ const reducer = (state = initialState, action) => {
 
     switch(action.type) {
         case 'CITY_WEATHER':
-            return {
-                ...state,
-                data: [...state.data, action.data]
+            
+            if(state.data.filter(data=> data.name === action.data.name).length > 0){
+                return {
+                    ...state
+                }
+            } else {
+                return {
+                    ...state,
+                    data: [...state.data, action.data]
+                    
+                }
             }
+            
         default:
             return state;
     }
